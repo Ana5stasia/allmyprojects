b91f2b89e455a5194edfc8e2cd0a535046061eb6
Ana5stasia <139070499+Ana5stasia@users.noreply.github.com>
2023-09-18 11:20:17 +0300

    Update The7th.jl

--- The7th.jl
+++ The7th.jl
@@ -1,8 +1,19 @@
 using HorizonSideRobots
 r=Robot(animate=true)
-while isborder(r, Nord)
-    move!(r, West)
-    if !isborder(r, Nord)
-        break
-    end
+
+fuction alongg!(r::Robot,side::HorizonSide,q::Integer)::Nothing
+    for i in 1:q
+    move!(r,side)
+    
+end
+function find_hole(r::Robot)::Nothing
+    side = Ost 
+    n=0
+    while isborder(r,Nord)
+        n+=1
+        alongg!(r,side,n)
+        side=inverse!(side)
+    end 
+    move!(r,Nord)
+    alongg!(r,side,n)
 end
