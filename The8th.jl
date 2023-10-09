d6524ae1fda3e27e53a978c3f6afe95b5a49fd43
Ana5stasia <139070499+Ana5stasia@users.noreply.github.com>
2023-10-09 14:37:04 +0300

    Create The8th.jl

---
+++ The8th.jl
@@ -0,0 +1,26 @@
+using HorizonSideRobots
+r=Robot(animate=true)
+
+function ag_clockwise_side(side::HorizonSide)::HorizonSide
+    return HorizonSide(mod(Int(side)+1,4))
+end
+
+function spiral(r::Robot)::Nothing
+    steps = 1
+    side = Nord
+    while ismarker(r)==false
+        for _ in 1:2
+            special_move!(r,side,steps)
+            side=ag_clockwise_side(side)
+        end
+        steps += 1
+    end
+end
+function special_move!(r::Robot,side::HorizonSide,num::Int)
+    for _ in 1:num
+        if ismarker(r)
+            return nothing
+        end
+        move!(r,side)
+    end
+end
